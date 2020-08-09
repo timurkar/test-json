394e0dd571b79d1dbeca8cab384a12b2ffe88ab9
Timur Karimbaev <timur@karimbaev.com>
2020-08-09 16:26:46 +0300

    Update tim.js

--- tim.js
+++ tim.js
@@ -1,3 +1,13 @@
 {
-	title: "Hello"
+	"success": true,
+	"data": {
+		"title": "01_Реклама",
+		"header": {
+			"compact": true
+		},
+		"layout": "stack",
+		"blocks": [
+			{ "type": "text", "text": "Hello Tim"}
+		]
+	}
 }
